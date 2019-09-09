fbf49a0dcb0076f49c25f3f63314e8a4d6df697a
obkeena <pamellpamill@gmail.com>
2019-09-09 07:27:20 +0700

    first commit

---
+++ src/Content.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { booleanTypeAnnotation } from '@babel/types';
+
+export default class Content extends Component{
+    constructor(){
+        super()
+        this.state={
+            count:0,
+            name:''
+        }
+    }
+
+    addNum=()=>{
+        this.setState({
+            count:this.state.count+1,
+            name:'test'
+        })
+    }
+    render(){
+        return(
+         <div>
+             name: {this.state.name}
+            <botton onClick={this.addNum}> count: {this.state.count}</botton>
+         </div> 
+        );
+
+    }
+
+}
